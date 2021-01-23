c27541947343798d271512dbd48e1c945183c588
sky <skybride@hotmail.com>
2021-01-23 17:26:46 -0500

    remove display for individual item

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ localStorage.setItem("items", JSON.stringify(listItemsArray));
 const listItems = JSON.parse(localStorage.getItem("items"))
 
 const createItem = inputText => {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.className = "btn btn-danger btn-sm";
+    deleteBtn.id = "delete";
+
     const li = document.createElement("li");
     li.textContent = inputText;
+    li.appendChild(deleteBtn);
     ul.appendChild(li);
 }
 
@@ -36,3 +42,14 @@ clearAll = () => {
     localStorage.clear();
     location.reload();
 }
+
+const deleteItem = document.getElementsByClassName("btn btn-danger btn-sm");
+for (let i = 0; i < deleteItem.length; i++){
+    deleteItem[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+        console.log(listItems);
+
+    }
+}
+
